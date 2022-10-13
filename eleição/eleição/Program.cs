d3415c72db32462a64f1f0036e749f434386c423
jamy165 <105940525+Nitierk@users.noreply.github.com>
2022-10-13 11:50:14 -0300

    projeto inicial

---
+++ eleição/eleição/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eleição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
