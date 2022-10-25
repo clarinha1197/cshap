e7fc3b735f2963cfbb177e477a29f19c7d09eafa
jamy165 <115711917+clarinha1197@users.noreply.github.com>
2022-10-25 10:25:13 -0300

    Update Program.cs

--- TipoByteInt/Program.cs
+++ TipoByteInt/Program.cs
@@ -6,7 +6,33 @@ namespace TipoByteInt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte v1 = 10;
+            Console.WriteLine("O Valor de V1 é: {0} ",v1);
+            Console.WriteLine("O tipo é: {0} ", v1.GetType());
+
+
+
+            Console.WriteLine("Digite um valor na faixa de 0 a 255");
+
+            try
+            {
+                var vdigitado = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("O valor digitado é byte");
+            }
+            catch
+            {
+                Console.WriteLine("O valor digitado não é byte");
+            }
+
+
+
+
+
+
+          
+       
+
+
         }
     }
 }
