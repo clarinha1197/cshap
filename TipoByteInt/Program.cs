73008206b7e4695ac45f4046da9a294a24494396
jamy165 <115711917+clarinha1197@users.noreply.github.com>
2022-10-25 10:24:00 -0300

    TipoBteInt

---
+++ TipoByteInt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TipoByteInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
